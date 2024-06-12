036ac6a63957c6232ba044ceda8b4ab0c2a1f8fd
Aluno <Aluno@LAB04-PC10>
2024-06-12 20:43:42 -0300

    Commit project Parsing

---
+++ src/Parsing01/Parsing01.java
@@ -0,0 +1,24 @@
+package Parsing01;
+
+public class Parsing01 {
+    public static void main(String[] args) {
+       
+        String shirtPrice = "15";
+        int intVar1 = Integer.parseInt(shirtPrice);
+        System.out.println(intVar1);
+        
+        String textRate = "0.05";
+        double doubleVar2 = Double.parseDouble(textRate);
+        System.out.println(doubleVar2);
+        
+        String gibberish = "88ds7nds87dsfs";
+        System.out.println(gibberish);
+        
+        
+        
+        
+     
+        
+    }
+    
+}
